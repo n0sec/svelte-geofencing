ec21fa89a1f98e8b7ab032345bc5ec380e7ca147
Nick <nconkl1@outlook.com>
2022-11-07 17:30:37 -0500

    title change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <header>
-    <title>Geofencing Test</title>
-    <nav class="bg-gray-700 drop-shadow-md px-6 mb-6 items-center align-middle grid grid-cols-2 h-14 gap-5">
-        <h1 class="text-2xl heading-title"><a href="/">Geofencing Test</a></h1>
-        <a href="about" class="col-end-4">About</a>
-        <div class="col-end-5">
+    <title>PlotD</title>
+    <nav class="bg-gray-700 drop-shadow-md px-6 mb-6 items-center align-middle justify-between flex h-14 gap-5">
+        <h1 class="text-2xl heading-title"><a href="/">PlotD</a></h1>
+        <div class="space-x-8">
+            <a href="about">About</a>
             <!-- TODO: Implement dark mode toggle-->
             <input type="checkbox" name="darkMode" id="darkMode">
         </div>

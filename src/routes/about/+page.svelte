5d8473f42dfa8fa112bdbfbe9da43473e43eb5b6
Nick Conklin <nconkl1@outlook.com>
2022-11-06 21:59:31 -0500

    updated about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,11 @@
-<div class="px-6">
+<div class="px-6 text-center">
+    <h1 class="text-2xl mb-3">About this app</h1>
     <p>This app can be used to plot geofenced punches onto a map. Enter a latitude, longitude, radius and press the "plot" button to get started</p>
     <p>This app was built by an independent Support Technician using a modern web framework and well known API</p>
-    <h1>Technologies Used:</h1>
-    <ul class="list-disc px-8">
+    <h1 class="mt-3">Technologies Used:</h1>
+    <ul>
         <li>SvelteKit</li>
-        <li>Vite</li>
+        <li>Typescipt</li>
         <li>TailwindCSS</li>
         <li>Google Maps API</li>
     </ul>

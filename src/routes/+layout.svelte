599985fdc3d67277af035e77c686f237bdc4a38b
Nick Conklin <nconkl1@outlook.com>
2022-11-06 21:44:16 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "../app.css";
+    const currentYear = new Date().getFullYear();
+</script>
+
+<header>
+    <title>Geofencing Test</title>
+    <nav class="bg-gray-700 drop-shadow-md px-6 mb-6 items-center align-middle grid grid-cols-2 h-14">
+        <h1 class="text-2xl heading-title"><a href="/">Geofencing Test</a></h1>
+        <a href="about" class="col-end-7">About</a>
+    </nav>
+</header>
+
+<main>
+    <slot></slot>
+</main>
+<div class="absolute bottom-0 w-full bg-gray-700 px-6 items-center align-middle grid grid-cols-2 h-14 drop-shadow-md">
+    <footer>&copy; {currentYear}</footer>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<div class="px-6">
+    <p>This app can be used to plot geofenced punches onto a map. Enter a latitude, longitude, radius and press the "plot" button to get started</p>
+    <p>This app was built by an independent Support Technician using a modern web framework and well known API</p>
+    <h1>Technologies Used:</h1>
+    <ul class="list-disc px-8">
+        <li>SvelteKit</li>
+        <li>Vite</li>
+        <li>TailwindCSS</li>
+        <li>Google Maps API</li>
+    </ul>
+</div>

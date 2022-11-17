5eaac2ac1d6bc8a74a70507004a7f128c1135e77
Nick <nconkl1@outlook.com>
2022-11-16 20:00:34 -0500

    updated POST endpoint

--- src/routes/api/share/+server.ts
+++ src/routes/api/share/+server.ts
@@ -4,12 +4,23 @@ import hash from 'object-hash';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const jsonResponse = await request.json();
-	const pointJson = JSON.parse(jsonResponse);
+	// Parse request Body into Object
+	const pointJson = await request.json();
 
-	const pointHash = hash.sha1(pointJson);
-	const stmt = db.prepare(`INSERT INTO points VALUES ('${pointHash}', '${pointJson}')`);
-	stmt.run();
+	// Stringify the object with JSON.stringify()
+	const pointString: string = JSON.stringify(pointJson);
 
+	// Hash the object using object-hash
+	const pointHash: string = hash.sha1(pointJson);
+
+	// Prepare the statement
+	const stmt = db.prepare(`INSERT INTO points VALUES (?, ?)`);
+
+	// Run the statement
+	stmt.run(pointHash, pointString);
+
+	// TODO: Create the new URL and return that instead
+
+	// Return a Response object with the hash in it
 	return new Response(pointHash);
 };

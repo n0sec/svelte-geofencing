c3dda962d57d9316d53bfb5fb5662bd84db414fc
Nick <nconkl1@outlook.com>
2022-11-17 12:46:00 -0500

    generated new URL with hash on server

--- src/routes/api/share/+server.ts
+++ src/routes/api/share/+server.ts
@@ -1,13 +1,13 @@
-import { error, json } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import db from '$lib/db';
 import hash from 'object-hash';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
-	// Why tf are we handling so many types? I don't know
-	if (request.body == null || undefined || '' || []) {
+	if (!request.body) {
 		throw error(400, 'Request body not found. Please try again.');
 	}
+
 	// Parse request Body into Object
 	const pointJson = await request.json();
 
@@ -15,7 +15,9 @@ export const POST: RequestHandler = async ({ request }) => {
 	const pointString: string = JSON.stringify(pointJson);
 
 	// Hash the object using object-hash
-	const pointHash: string = hash.sha1(pointJson);
+	const pointHash: string = hash(pointJson, { algorithm: 'md5' });
+	const newUrl = `${request.url}/${pointHash}`;
+	console.log(newUrl);
 
 	try {
 		// Prepare the statement
@@ -28,7 +30,7 @@ export const POST: RequestHandler = async ({ request }) => {
 	}
 
 	// Return a Response object with the hash in it
-	return new Response(pointHash);
+	return new Response(newUrl);
 
 	// TODO: Create the new URL and return that instead
 };

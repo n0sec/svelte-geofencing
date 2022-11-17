ebc478d7175bd47eb757cc037f61edf301bdf77a
Nick <nconkl1@outlook.com>
2022-11-17 11:29:42 -0500

    handled invalid body

--- src/routes/api/share/+server.ts
+++ src/routes/api/share/+server.ts
@@ -4,6 +4,10 @@ import hash from 'object-hash';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
+	// Why tf are we handling so many types? I don't know
+	if (request.body == null || undefined || '' || []) {
+		throw error(400, 'Request body not found. Please try again.');
+	}
 	// Parse request Body into Object
 	const pointJson = await request.json();
 
@@ -13,14 +17,18 @@ export const POST: RequestHandler = async ({ request }) => {
 	// Hash the object using object-hash
 	const pointHash: string = hash.sha1(pointJson);
 
-	// Prepare the statement
-	const stmt = db.prepare(`INSERT INTO points VALUES (?, ?)`);
+	try {
+		// Prepare the statement
+		const stmt = db.prepare(`INSERT INTO points VALUES (?, ?)`);
 
-	// Run the statement
-	stmt.run(pointHash, pointString);
-
-	// TODO: Create the new URL and return that instead
+		// Run the statement
+		stmt.run(pointHash, pointString);
+	} catch (error: any) {
+		throw error(500, 'Something went wrong with the request. Please try the request again.');
+	}
 
 	// Return a Response object with the hash in it
 	return new Response(pointHash);
+
+	// TODO: Create the new URL and return that instead
 };

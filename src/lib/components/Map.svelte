2a95ecc12a32946159ad762d4ec83454c2a59647
Nick Conklin <nconkl1@outlook.com>
2022-11-23 08:27:50 -0500

    adjusted Map height

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -83,4 +83,4 @@
 	}
 </script>
 
-<div id="map" class="overflow-hidden col-span-3 row-span-3 h-5/6 z-10" bind:this={mapElement} />
+<div id="map" class="overflow-hidden col-span-3 row-span-3 h-[90vh] z-10" bind:this={mapElement} />

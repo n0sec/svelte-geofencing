8dd9ca73e17b265fb7e676a65779dbfe2faa8fa7
Nick <nconkl1@outlook.com>
2022-11-23 17:15:19 -0500

    added variable that stores the point_string as JSON

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -2,6 +2,9 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let points = JSON.parse(data.result.point_string);
+	console.log(typeof points);
 </script>
 
-<h1>{data.id}</h1>
+<h1>{points}</h1>

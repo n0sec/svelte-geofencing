630ee66c6de7b79cb55dfa92d6656b4f1ead5ee3
Nick Conklin <nconkl1@outlook.com>
2022-11-24 16:40:40 -0500

    updated error page

--- src/routes/[id]/+error.svelte
+++ src/routes/[id]/+error.svelte
@@ -2,4 +2,11 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>{$page.error?.message}</h1>
+<div class="container mx-auto flex justify-center flex-col h-screen items-center">
+	<h1 class="text-9xl">{$page.status}</h1>
+	<p class="text-neutral-100/50 text-2xl mt-4">{$page.error?.message}</p>
+	<button
+		class="button uppercase p-5 mt-6 rounded-lg shadow-md bg-gray-600 hover:bg-gray-700 font-semibold"
+		type="button"><a href="/">Go Home</a></button
+	>
+</div>

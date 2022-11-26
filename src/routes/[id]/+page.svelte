8d37988d5d9d0c9266121b9bc4c3d2fd34af367b
Nick Conklin <nconkl1@outlook.com>
2022-11-25 19:33:02 -0500

    wip - added logic to plot points from db on map

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Map from '$lib/components/Map.svelte';
-	import type { SvelteComponentTyped } from 'svelte';
+	import { onMount, type SvelteComponentTyped } from 'svelte';
+	import { browser } from '$app/environment';
+	import type { PlotCircle } from '$lib/types/PlotCircle';
 
 	export let data: PageData;
 
 	let points = JSON.parse(data.result.point_string);
 
-	let mapRef: SvelteComponentTyped;
+	let mapRef: SvelteComponentTyped | HTMLElement;
 	let errorText: string;
+	let map: L.Map;
+
+	onMount(async () => {
+		if (browser) {
+			const L = await import('leaflet');
+
+			map = L.map(mapRef as HTMLElement);
+
+			points.forEach((point: PlotCircle) => {
+				L.circle([point.latitude as number, point.longitude as number], {
+					color: point.color as string,
+					radius: point.radius as number
+				}).addTo(map);
+				map.fitWorld();
+			});
+		}
+	});
 </script>
 
 <Map bind:this={mapRef} />

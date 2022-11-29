cc442372dd8ede9bc8aaf476ac78821ea20133aa
Nick <nconkl1@outlook.com>
2022-11-29 14:05:51 -0500

    added check for expired share codes

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -18,6 +18,31 @@ export const load: PageServerLoad = async ({ params }) => {
 		});
 	}
 
+	// Set the number of milliseconds in 24 hours
+	const oneDay: number = 86400000;
+
+	// Calculate the difference between when the URL was accessed (now) and the creation_date in the db when the record was created
+	const timeDifference: number = Date.now() - result.creation_date;
+
+	// If the difference is > 24 hours
+	if (timeDifference > oneDay) {
+		// Try to delete from the db
+		try {
+			const deleteStmt = db.prepare(`DELETE FROM points WHERE id=(?)`);
+			deleteStmt.run(params.id);
+			// Catch where anything random could go wrong
+			// Throw a 500
+		} catch (err) {
+			throw error(500, {
+				message: `Some unknown error occurred ${err}`
+			});
+		}
+		// If the record was deleted successfully, throw a 500 letting the user know the record was deleted
+		throw error(500, {
+			message: 'The code you are trying to access has expired.'
+		});
+	}
+
 	return {
 		result
 	};

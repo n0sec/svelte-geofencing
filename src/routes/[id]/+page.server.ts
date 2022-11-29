2ae134d37f8cff288c04aebf88f5ec98a068b1bc
Nick <nconkl1@outlook.com>
2022-11-29 13:31:42 -0500

    renamed back to page for now

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,13 +1,14 @@
-import type { LayoutServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 import db from '$lib/server/db';
 import { error } from '@sveltejs/kit';
 
-export const load: LayoutServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params }) => {
 	// Select the [id] from the db
 	// ? Verbose still prints out here
 	const stmt = db.prepare(`SELECT * FROM points WHERE id=(?)`);
 
 	// Get the row for [id]
+	// Returns undefined if nothing was found
 	const result = stmt.get(params.id);
 
 	// If it doesn't exist, throw a 404

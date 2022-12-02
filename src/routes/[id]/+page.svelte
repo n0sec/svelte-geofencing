5a2fadfee6347eef6a2d1695e57bbcf825267310
Nick <nconkl1@outlook.com>
2022-12-02 16:18:12 -0500

    added styling to table

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -68,8 +68,12 @@
 						<td class="py-3 px-6">{mapPoint.longitude}</td>
 						<td class="py-3 px-6">{mapPoint.radius}</td>
 						<td class="py-3 px-6">{mapPoint.note ?? 'None'}</td>
-						<td class="py-3 px-6">{mapPoint.color}</td>
-					</tr>
+						<td class="py-3 px-6"
+							><div class="w-4 h-4" style:background-color={mapPoint.color} />
+							({mapPoint.color})</td
+						>
+						<td /></tr
+					>
 				{/each}
 			</tbody>
 		</table>

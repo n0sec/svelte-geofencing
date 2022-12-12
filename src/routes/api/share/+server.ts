8b00ef2581461fce43131108bf2a6dbb63cffcdb
Nick <nconkl1@outlook.com>
2022-12-12 12:50:36 -0500

    refactored using OpenAI

--- src/routes/api/share/+server.ts
+++ src/routes/api/share/+server.ts
@@ -3,18 +3,13 @@ import db from '$lib/server/db';
 import { customAlphabet } from 'nanoid';
 import type { RequestHandler } from './$types';
 
+const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 15);
+
 export const POST: RequestHandler = async ({ request }) => {
 	// Parse request Body into Object
 	const body = await request.json();
 
-	// Stringify the object with JSON.stringify()
-	const pointString: string = JSON.stringify(body);
-
 	// Generate a unique identifier to match with the points
-	const nanoid = customAlphabet(
-		'1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-		15
-	);
 	const id: string = await nanoid();
 
 	// Generate the creation_date column using Date
@@ -27,7 +22,7 @@ export const POST: RequestHandler = async ({ request }) => {
 		const stmt = db.prepare(`INSERT INTO points VALUES (?, ?, ?)`);
 
 		// Run the statement
-		stmt.run(id, pointString, now);
+		stmt.run(id, JSON.stringify(body), now);
 
 		// Return a Response object with the identifier in it
 		return new Response(id);
